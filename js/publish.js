37c8789c76e05d638d1e1f7d66b2ef819189294a
Neeraj Suthar <neerajsuthar@SavvyMobs-MacBook-Pro.local>
2018-08-07 16:57:07 +0530

    debug

--- js/publish.js
+++ js/publish.js
@@ -18,22 +18,29 @@ const createTopic = async (topicName) => {
 const publishMessage = async (topicName, data) => {
   const allTopics = await listAllTopics();
   let nodeEnv = process.env.NODE_ENV;
+  console.log(nodeEnv)
   if (process.env.POD_NAMESPACE && process.env.POD_NAMESPACE !== "live") {
     nodeEnv = "beta";
   }
+  console.log(nodeEnv)
   let fullTopicName = `${topicName}-${nodeEnv}`;
   if (nodeEnv !== "production") {
     fullTopicName = `${fullTopicName}-${process.env.LOGNAME}-${process.env.TRAVIS_BRANCH}-${process.env.POD_NAMESPACE}`;
   }
+  console.log(fullTopicName)
   let topic = allTopics.find(t => t.name.split("/").slice(-1).pop() === fullTopicName);
   if (!topic) {
     topic = await createTopic(fullTopicName);
   }
+  console.log(topic)
   const publisher = topic.publisher();
   // Publishes the message as a string to the topic
+  console.log(publisher)
   const dataBuffer = Buffer.from(JSON.stringify(data));
+  console.log(dataBuffer)
   try {
     const results = await publisher.publish(dataBuffer);
+    console.log(results)
     const messageId = results[0];
     console.log(`Message ${messageId} published.`);
     return messageId;
